ebeda7bec3550baff93a4db19f153b6a6b980655
Bob <shixiaobao17145@163.com>
2018-08-29 03:17:18 +0800

    add git by clone

--- download-git.js
+++ download-git.js
@@ -1,4 +1,6 @@
 let downloadUrl = require('download');
+var gitclone = require('git-clone');
+var rm = require('rimraf').sync;
 
 module.exports = {
 	fetchGit:function(url, branchOrTag = 'master', dest, isSSH){
@@ -9,8 +11,18 @@ module.exports = {
 			return this.downloadByClone(repo, dest);
 		}
 	},
-	downloadByClone:function(){
-
+	downloadByClone:function(repo, dest){
+		let resolve,reject, promise = new Promise((rs,rj)=>(resolve = rs, reject=rj));
+		console.log('exec git clone with url:' + repo.sshUrl + ', branchOrTag:' + repo.branchOrTag);
+		gitclone(repo.sshUrl, dest, { checkout: repo.branchOrTag, shallow: repo.branchOrTag == 'master' }, function (err) {
+			if (err === undefined) {
+				rm(dest + '/.git')
+				resolve('success');
+			} else {
+				reject(err)
+			}
+		});
+		return promise;
 	},
 	url2Repo:function(url, branchOrTag){
 		let reg = /((f|ht)tps?:\/\/[^\/]*\/|\w+@[^:]*?:)?([^\/]*\/[^\/]*?)(\.[^\/]*)?$/i;
@@ -23,21 +35,24 @@ module.exports = {
 			branchOrTag
 		}
 		if(!matches[2]){
+			repo.sshUrl = url;
 			let rawOrigin = origin.split(/@|:/)[1];
 			origin = 'https://' + rawOrigin +'/';
 			repo.rawOrigin = rawOrigin;
-			repo.type = 'ssh';
 		}else{
 			let protocolIdx = origin.indexOf(':');
-			repo.rawOrigin = origin.substring(protocolIdx+3, origin.length - 2);
+			repo.rawOrigin = origin.substring(protocolIdx+3, origin.length - 1);
+			repo.sshUrl = 'git@'+repo.rawOrigin+':'+owerAndName+'.git';
 		}
-		let uri = origin + owerAndName + '/archive/' + branchOrTag + '.zip';
-		repo.uri = uri;
+		let repoUrl = origin + owerAndName + '.git';
+		repo.repoUrl = repoUrl;
+		let zipUrl = origin + owerAndName + '/archive/' + branchOrTag + '.zip';
+		repo.zipUrl = zipUrl;
 		return repo;
 	},
 	downloadZip:function (repo, dest){
-		console.log('downloading:' + repo.uri);
-		return downloadUrl(repo.uri, dest, { extract: true, strip: 1, mode: '666', headers: { accept: 'application/zip' } }).then(()=>repo).catch(err=>{err.repo = repo; throw err});
+		console.log('downloading:' + repo.zipUrl);
+		return downloadUrl(repo.zipUrl, dest, { extract: true, strip: 1, mode: '666', headers: { accept: 'application/zip' } }).then(()=>repo).catch(err=>{err.repo = repo; throw err});
 	}
 }
 
